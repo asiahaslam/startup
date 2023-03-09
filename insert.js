ac15c870876e207762c0432a8b9b7cfec91a059b
Asia Haslam <asiakhaslam@gmail.com>
2023-03-08 18:13:50 -0700

    implemented name function on first page

--- insert.js
+++
@@ -1,6 +0,0 @@
-const playerNameEl = document.getElementById("user");
-playerNameEl.textContent = this.getPlayerName();
-
-getPlayerName() {
-  return localStorage.getItem('userName') ?? 'Mystery player';
-}
--- name.js
+++
@@ -1,5 +0,0 @@
-function personName () {
-    const nameEl = document.querySelector("#names");
-    localStorage.setItem("userName", nameEl.value);
-    window.location.href = 'story-1.html';
-}
